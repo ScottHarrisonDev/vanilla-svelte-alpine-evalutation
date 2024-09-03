d16270042e5a3d5dab86d6bd3c12a94f4ade86f5
Scott Harrison <github@scottharrisondesign.co.uk>
2024-09-03 17:06:32 +0100

    initial build with scoped JS example

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), alpinejs()]
+});
---
+++ src/components/scoped-js/svelte.svelte
@@ -0,0 +1,11 @@
+<script>
+    let checked = false
+</script>
+
+<div class="scoped-js-component">
+    Clicking the button below should only toggle the checkbox in this specific instance of the <code>&ltSvelte&gt</code> component.
+    <div class="scoped-js-component__interactive-area">
+        <button type="button" class="button" on:click={() => checked = !checked}>Click Here</button>
+        <input type="checkbox" value="vanilla" bind:checked={checked} />
+    </div>
+</div>
